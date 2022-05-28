4de71ce7c5f822ace37c6bc5b3962ebc3a4994ff
Mykhailo <pmtsmichael@gmail.com>
2022-05-28 19:15:51 +0300

    [SePr-03] add rabbitmq sender(producer)

--- os-animenews-producer/src/main/java/os/animenews/com/application/Main.java
+++ os-animenews-producer/src/main/java/os/animenews/com/application/Main.java
@@ -1,23 +1,31 @@
 package os.animenews.com.application;
 
 
+import os.animenews.model.Article;
 import os.animenews.pageObject.AnimeNewsBO;
+import os.animenews.rabbitmq.Sender;
 import os.animenews.utils.CustomDriver;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     private final static String ROOT_URL = "https://www.cbr.com/category/anime-news/";
 
     public static void main(String[] args) {
-       try {
-           CustomDriver.initDriver();
+        List<Article> articleList = new ArrayList<>();
 
-           new AnimeNewsBO()
-                   .proceedToAnimeNewsCategoryPage()
-                   .getArticleBlocks();
-       }
-       finally {
-           CustomDriver.quitDriver();
-       }
+        try {
+            CustomDriver.initDriver();
+
+            articleList = new AnimeNewsBO()
+                    .proceedToAnimeNewsCategoryPage()
+                    .getArticleBlocks();
+        } finally {
+            CustomDriver.quitDriver();
+        }
+
+        Sender.sendToQ(articleList);
     }
 
 }
---
+++ os-animenews-producer/src/main/java/os/animenews/rabbitmq/Sender.java
@@ -0,0 +1,36 @@
+package os.animenews.rabbitmq;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import os.animenews.model.Article;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.TimeoutException;
+
+public class Sender {
+    private final static String QUEUE_NAME = "qtosql";
+
+    public static void sendToQ(List<Article> articleList) {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        try (Connection connection = factory.newConnection();
+             Channel channel = connection.createChannel()) {
+
+            channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+
+            for(Article article : articleList){
+                channel.basicPublish("", QUEUE_NAME,null, article.toBytes());
+            }
+
+            //System.out.println(" [x] Sent '" + message + "'");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (TimeoutException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
